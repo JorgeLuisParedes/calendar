dfe01364c299b16f8ee4956224769851b12753d5
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-27 22:20:46 +0100

    correcion metodo del renew

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -45,9 +45,11 @@ export const useAuthStore = () => {
 
 	const checkAuthToken = async () => {
 		const token = localStorage.getItem('token');
-		if (!token) return dispatch(onLogout());
+		if (!token) {
+			return dispatch(onLogout());
+		}
 		try {
-			const { data } = await calendarApi.post('/auth/renew');
+			const { data } = await calendarApi.get('/auth/renew');
 			localStorage.setItem('token', data.token);
 			localStorage.setItem('token-init-date', new Date().getTime());
 			dispatch(onLogin({ name: data.name, uid: data.uid }));
