6295be2e132bf0016b1aeec3f553c3a26f76cc4d
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-28 01:26:36 +0100

    limpiar informacion del calendario

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -53,14 +53,20 @@ export const calendarSlice = createSlice({
 				}
 			});
 		},
+		onLogoutCalendar: state => {
+			state.isLoadignEvents = true;
+			state.events = [];
+			state.activeEvent = null;
+		},
 	},
 });
 
 // Action creators are generated for each case reducer function
 export const {
-	onSetActiveEvent,
 	onAddNewEvent,
-	onUpdateEvent,
 	onDeleteEvent,
 	onLoadEvents,
+	onLogoutCalendar,
+	onSetActiveEvent,
+	onUpdateEvent,
 } = calendarSlice.actions;
